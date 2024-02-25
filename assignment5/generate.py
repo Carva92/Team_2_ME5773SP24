0e9a5ea40af44e142523393ec30968191a718d2c
Roberto Enriquez Vargas <roberto.enriquezvargas@my.utsa.edu>
2024-02-25 15:06:20 -0600

    modified generate.py to save CSV and npy files

--- assignment5/generate.py
+++ assignment5/generate.py
@@ -53,6 +53,132 @@ E = np.arange(350.0,350.4,0.1,dtype=np.float32).reshape((2,2), order='C')
 
 
 
+#start time for time to save A to csv
+start_time_A_CSV = time.time()
+
+#save file A.CSV
+np.savetxt('A.CSV',A, fmt='%d', delimiter=',')
+
+# evaluate calculation time for A
+A_time_CSV = time.time() - start_time_A_CSV
+
+print("The time to create A.CSV is : ", end="")
+print('%.6f' % A_time_CSV)
+
+#start time for time to save B to CSV
+start_time_B_CSV = time.time()
+
+#save file B.CSV
+np.savetxt('B.CSV',B, fmt='%d', delimiter=',')
+
+# evaluate calculation time for B
+B_time_CSV = time.time() - start_time_B_CSV
+
+print("The time to create B.CSV is : ", end="")
+print('%.6f' % B_time_CSV)
+
+#start time for time to save C to CSV
+start_time_C_CSV = time.time()
+
+#save file C.CSV
+np.savetxt('C.CSV',C, fmt='%.18e', delimiter=',')
+
+# evaluate calculation time for C
+C_time_CSV = time.time() - start_time_C_CSV
+
+print("The time to create C.CSV is : ", end="")
+print('%.6f' % C_time_CSV)
+
+#start time for time to save D to CSV
+start_time_D_CSV = time.time()
+
+#save file D.CSV
+np.savetxt('D.CSV',D, fmt='%d', delimiter=',')
+
+# evaluate calculation time for D
+D_time_CSV = time.time() - start_time_D_CSV
+
+print("The time to create D.CSV is : ", end="")
+print('%.6f' % D_time_CSV)
+
+#start time for time to save E to CSV
+start_time_E_CSV = time.time()
+
+#save file E.CSV
+np.savetxt('E.CSV',E, fmt='%.7e', delimiter=',')
+
+# evaluate calculation time for E
+E_time_CSV = time.time() - start_time_E_CSV
+
+print("The time to create E.CSV is : ", end="")
+print('%.6f' % E_time_CSV)
+
+print()
+
+#start time for time to save A to .npy
+start_time_A_npy = time.time()
+
+#save file A.npy
+np.save('A.npy',A)
+
+# evaluate calculation time for A
+A_time_npy = time.time() - start_time_A_npy
+
+print("The time to create A.npy is : ", end="")
+print('%.6f' % A_time_npy)
+
+#start time for time to save B to .npy
+start_time_B_npy = time.time()
+
+#save file B.npy
+np.save('B.npy',B)
+
+# evaluate calculation time for B
+B_time_npy = time.time() - start_time_B_npy
+
+print("The time to create B.npy is : ", end="")
+print('%.6f' % B_time_npy)
+
+#start time for time to save C to .npy
+start_time_C_npy = time.time()
+
+#save file C.npy
+np.save('C.npy',C)
+
+# evaluate calculation time for C
+C_time_npy = time.time() - start_time_C_npy
+
+print("The time to create C.npy is : ", end="")
+print('%.6f' % C_time_npy)
+
+#start time for time to save D to .npy
+start_time_D_npy = time.time()
+
+#save file D.npy
+np.save('D.npy',D)
+
+# evaluate calculation time for D
+D_time_npy = time.time() - start_time_D_npy
+
+print("The time to create D.npy is : ", end="")
+print('%.6f' % D_time_npy)
+
+#start time for time to save E to .npy
+start_time_E_npy = time.time()
+
+#save file E.npy
+np.save('E.npy',E)
+
+# evaluate calculation time for E
+E_time_npy = time.time() - start_time_E_npy
+
+print("The time to create E.npy is : ", end="")
+print('%.6f' % E_time_npy)
+print()
+
+
+
+
 
 
 
