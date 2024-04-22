5d4c5529450e6d25b702fb9d47bde2ea2ef2507d
Julian Carvajal <jcarvajalrico@gmail.com>
2024-04-21 22:08:56 -0500

    assignment10 folder

---
+++ assignment10/searchutils.f90
@@ -0,0 +1,79 @@
+module searchutils
+    implicit none
+
+contains
+
+! Description: Function that finds the location (idx) of a value x
+! in an array using the linear search algorithm.
+!
+! Find idx such that arr(idx) == x
+!
+FUNCTION linearSearch(arr, n, x) RESULT(idx)
+
+ IMPLICIT NONE
+
+ REAL(8) :: arr(n) ! Array to search
+ INTEGER :: n ! Number of elements in array.
+ REAL(8) :: x ! Value to search for in array.
+ INTEGER :: idx ! Result of the search. [arr(idx) == x]
+
+ ! Your implementation here.
+
+ INTEGER :: i
+        
+        !set idx to -1 in case element is not found.
+    idx = -1 
+
+    DO i = 1, n
+        IF (arr(i) == x) THEN
+
+                !update idx if element is found
+            idx = i 
+            EXIT
+        END IF
+    END DO
+
+
+
+
+END FUNCTION linearSearch
+! Description: Function that finds the location (idx) of a value x
+! in a sorted array using the binary search algorithm.
+!
+! Find idx such that arr(idx) == x
+!
+FUNCTION binarySearch(arr, n, x) RESULT(idx)
+
+ IMPLICIT NONE
+
+ REAL(8) :: arr(n) ! Array to search
+ INTEGER :: n ! Number of elements in array.
+ REAL(8) :: x ! Value to search for in array.
+ INTEGER :: idx ! Result of the search. [arr(idx) == x]
+
+ ! Your implementation here.
+ INTEGER :: low, high, mid
+    
+    low = 1
+    high = n
+    idx = -1
+    
+    !binary search
+    DO WHILE (low <= high)
+        mid = (low + high) / 2
+        IF (arr(mid) == x) THEN
+            idx = mid
+            EXIT
+        ELSE IF (arr(mid) < x) THEN
+            low = mid + 1
+        ELSE
+            high = mid - 1
+        END IF
+    END DO
+
+
+
+END FUNCTION binarySearch
+
+
+end module searchutils
